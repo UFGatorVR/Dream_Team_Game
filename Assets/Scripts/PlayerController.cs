1390dc522158f9eed0a262a786a8ecc3ebc8f059
zhabib3 <zhabib@ufl.edu>
2018-07-24 18:13:07 -0400

    Added Assets Folders, PlayerController and Trigger Scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 10f;
+    public float turnSpeed = 5f;
+    Transform mainCamera;
+    Quaternion playerRotation;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mainCamera = Camera.main.transform;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+
+        // Determine direction
+        Vector3 direction = new Vector3(mainCamera.transform.forward.x, mainCamera.transform.forward.y , mainCamera.transform.forward.z).normalized * turnSpeed * Time.deltaTime;
+
+        // Determine Rotation
+        Quaternion rotation = Quaternion.Euler(new Vector3(0, -transform.rotation.eulerAngles.y, 0));
+
+        // Move the player
+        transform.Translate(rotation * direction);
+
+    }
+
+    
+}
---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+
+    private int score;
+	// Use this for initialization
+	void Start () {
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+    }
+
+    // Check if player collides with pickups
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+        }
+        Debug.Log("Score: " + score.ToString());
+    }
+}
